d9c571332922a2e040f8fb5eb43297b3047e523d
AdrienW86 <adrien_weiss@outlook.fr>
2025-06-20 13:39:05 +0200

    mise en ligne

--- components/PhoneButton/PhoneButton.jsx
+++ components/PhoneButton/PhoneButton.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from './phone.module.css'
 
 export default function PhoneButton() {
   return (
-    <a href="tel:+33628286967" className={styles.link}>
+    <Link href="tel:+33628286967" className={styles.link}>
       <p> 📞 06.28.28.69.67</p>                        
-    </a>
+    </Link>
   )
 }
